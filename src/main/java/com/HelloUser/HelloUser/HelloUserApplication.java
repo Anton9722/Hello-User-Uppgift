0380437f9bdf0050e9100ae9eda4087772933431
Anton Johansson <anton9797johansson@hotmail.com>
2023-10-28 12:02:45 +0200

    fixed add new member bugg

--- src/main/java/com/HelloUser/HelloUser/HelloUserApplication.java
+++ src/main/java/com/HelloUser/HelloUser/HelloUserApplication.java
@@ -1,11 +1,20 @@
 package com.HelloUser.HelloUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloUserApplication {
 
+	public static List<Members> membersList = new ArrayList<>();
+	static{
+		membersList.add(new Members("Kalle", "Kalle@hotmail.com", 32));
+		membersList.add(new Members("Lisa", "Lisa@gmail.com", 29));
+		membersList.add(new Members("Hannes", "Hannes@hotmail.com", 40));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloUserApplication.class, args);
 	}
---
+++ src/main/java/com/HelloUser/HelloUser/HelloUserController.java
@@ -0,0 +1,42 @@
+package com.HelloUser.HelloUser;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class HelloUserController {
+    
+    @GetMapping("/")
+    String getStartPage() {
+        return "startpage";
+    }
+
+    @GetMapping("/members")
+    String getMembersPage(Model model) {
+        model.addAttribute("membersList", HelloUserApplication.membersList);
+        return "members";
+    }
+
+    @GetMapping("/addnewmember")
+    String getAddNewMemberPage(Model model) {
+        model.addAttribute("newMember", new Members(null, null, 0));
+        return "addnewmember";
+    }
+
+    @PostMapping("/new-member")
+    String newMember(@ModelAttribute Members newMembers, Model model) {
+        if (newMembers.getEmail() == null || newMembers.getName() == null || newMembers.getAge() <= 0) {
+            System.out.println("Felaktig inmatning");
+        }else{
+            HelloUserApplication.membersList.add(newMembers);
+        }
+        return "redirect:/addnewmember";
+    }
+    
+
+}
